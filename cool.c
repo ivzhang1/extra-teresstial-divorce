3e8269072b59bd52b14700d7f3de7fe6dbdb5509
ivzhang1 <izhang1@stuy.edu>
2018-11-14 22:50:40 -0500

    testing ls

--- cool.c
+++ cool.c
@@ -18,9 +18,9 @@ int main(){
   char ls[] = "ls -l -a";
   char gt[] = "git commit -a -m 'my_program_did_this_commit_for_me_this_is_so_epic'";
   char** args = parse_args(sl);
-  arg_printer(args);
-  execvp(args[0], args);
-  free(args);
+  /* arg_printer(args); */
+  /* execvp(args[0], args); */
+  /* free(args); */
   args = parse_args(ls);
   arg_printer(args);
   execvp(args[0], args);
